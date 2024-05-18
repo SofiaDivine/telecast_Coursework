f47e9e1a19bedb4761b0adf6581e50998106a6a7
Sofia <sonimavispc@gmail.com>
2024-05-18 23:18:19 +0300

    added sections to all channels

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+$('#editChannelDropdown').on("click", function (e) {
+  $('#editChannelMenu').toggle();
+  $('#editScheduleMenu').hide();
+  e.stopPropagation();
+  e.preventDefault();
+});
+$('#editScheduleDropdown').on("click", function (e) {
+  $('#editScheduleMenu').toggle();
+  $('#editChannelMenu').hide();
+  e.stopPropagation();
+  e.preventDefault();
+});
 
+$('.filter-category').on('click', function (e) {
+  e.preventDefault();
+  var category = $(this).attr('data-category');
+
+  if (category === 'all') {
+    $('.channel-card').show();
+  } else {
+    $('.channel-card').each(function () {
+      var channelCategory = $(this).attr('data-category');
+      if (channelCategory === category) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  }
+});
