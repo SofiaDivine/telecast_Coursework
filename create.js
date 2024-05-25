0cd43ff5472036d35c850dd57722c57125d48559
Sofia <sonimavispc@gmail.com>
2024-05-25 13:29:35 +0300

    imported categories to create.js

--- create.js
+++ create.js
@@ -1,80 +1,27 @@
-import { channels } from './channels.js';
+import categories from "./categories.js";
 
 const form = document.forms[0];
 const channels = [];
 const schedules = [];
+
+// Заповнення селектора категорій
+const categorySelect = form.querySelector('#category');
+
+// Очищення селектора перед додаванням категорій
+categorySelect.innerHTML = '';
+
+categories.forEach(category => {
+  const option = document.createElement('option');
+  option.value = category;
+  option.textContent = category;
+  categorySelect.appendChild(option);
+});
 
 loadChannels();
 loadSchedules();
 
 form.onsubmit = handleSubmit;
 
-/*         <form>
-            <div class="form-group">
-                <label for="channelImage">Фото каналу:</label>
-                <input type="file" class="form-control-file" id="channelImage">
-            </div>
-            <div class="form-group">
-                <label for="channelName">Назва каналу:</label>
-                <input type="text" class="form-control" id="channelName" placeholder="Введіть назву каналу">
-            </div>
-            <div class="form-group">
-                <label for="category">Категорія:</label>
-                <select class="form-control" id="category">
-                    <option>Новини</option>
-                    <option>Спорт</option>
-                 <option>Фільми і Серіали</option>
-                    <option>Розважальні</option>
-                    <option>Пізнавальні</option>
-                    <option>Дитячі</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="schedule">Розклад програми:</label>
-                <div class="row">
-                    <div class="col-md-6">
-                        <input type="text" class="form-control mb-2" placeholder="Час" name="time">
-                    </div>
-                    <div class="col-md-6">
-                        <input type="text" class="form-control mb-2" placeholder="Опис програми" name="description">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6">
-                        <input type="text" class="form-control mb-2" placeholder="Час" name="time">
-                    </div>
-                    <div class="col-md-6">
-                        <input type="text" class="form-control mb-2" placeholder="Опис програми" name="description">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6">
-                        <input type="text" class="form-control mb-2" placeholder="Час" name="time">
-                    </div>
-                    <div class="col-md-6">
-                        <input type="text" class="form-control mb-2" placeholder="Опис програми" name="description">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6">
-                        <input type="text" class="form-control mb-2" placeholder="Час" name="time">
-                    </div>
-                    <div class="col-md-6">
-                        <input type="text" class="form-control mb-2" placeholder="Опис програми" name="description">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6">
-                        <input type="text" class="form-control mb-2" placeholder="Час" name="time">
-                    </div>
-                    <div class="col-md-6">
-                        <input type="text" class="form-control mb-2" placeholder="Опис програми" name="description">
-                    </div>
-                </div>
-            </div>
-            <button type="submit" class="btn btn-primary mb-5">Створити канал</button>
-        </form>
- */
 function handleSubmit(e) {
   e.preventDefault();
   const formData = new FormData(form);
@@ -139,22 +86,4 @@ $('#editScheduleDropdown').on("click", function (e) {
   $('#editChannelMenu').hide();
   e.stopPropagation();
   e.preventDefault();
-});
-
-$('.filter-category').on('click', function (e) {
-  e.preventDefault();
-  var category = $(this).attr('data-category');
-
-  if (category === 'all') {
-    $('.channel-card').show();
-  } else {
-    $('.channel-card').each(function () {
-      var channelCategory = $(this).attr('data-category');
-      if (channelCategory === category) {
-        $(this).show();
-      } else {
-        $(this).hide();
-      }
-    });
-  }
 });
